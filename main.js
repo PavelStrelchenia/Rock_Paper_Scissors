b55d4fe3f428b5b095deb3af3a8fd6c7da4e32b1
PavelStrelchenia <sprrps@icloud.com>
2026-02-08 11:26:25 +0300

    Add getHumanChoice function

--- main.js
+++ main.js
@@ -3,10 +3,15 @@
 function getComputerChoice() {
   let randomNum = Math.random();
   if (randomNum < 0.3) {
-    return "Rock";
+    return "rock";
   } else if (randomNum > 0.3 && randomNum < 0.7) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
+}
+
+function getHumanChoice() {
+  let choiceFromPrompt = prompt("Enter your choice (rock, paper, scissors): ");
+  return choiceFromPrompt;
 }
