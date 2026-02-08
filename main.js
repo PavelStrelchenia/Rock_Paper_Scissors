e9cdbdbc33c4a73ebc2a301d7d5e75fbb24ae978
PavelStrelchenia <sprrps@icloud.com>
2026-02-08 11:21:56 +0300

    Add getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function getComputerChoice() {
+  let randomNum = Math.random();
+  if (randomNum < 0.3) {
+    return "Rock";
+  } else if (randomNum > 0.3 && randomNum < 0.7) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
