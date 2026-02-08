628ae69aabbd831387be557d925b18822550850e
PavelStrelchenia <sprrps@icloud.com>
2026-02-08 12:43:48 +0300

    Add playGame function

--- main.js
+++ main.js
@@ -12,9 +12,51 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let choiceFromPrompt = prompt("Enter your choice (rock, paper, scissors): ");
+  let choiceFromPrompt = prompt(
+    "Enter your choice (rock, paper, scissors): ",
+  ).toLowerCase();
   return choiceFromPrompt;
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    humanScore++;
+    computerScore++;
+    console.log("Its a tie!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  }
+}
+
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`${humanScore} : ${computerScore}`);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("You won!");
+  } else if (humanScore < computerScore) {
+    console.log("You lose!");
+  } else {
+    console.log("Tie!");
+  }
+}
+
+playGame();
